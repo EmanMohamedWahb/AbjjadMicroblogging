aec9e5620b39c52fe02fb88d121b3189c95b47c4
eman.mohamed@successgate.net <eman.mohamed@successgate.net>
2024-11-30 23:02:33 +0200

    Create the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CheckboxModule} from 'primeng/checkbox';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
